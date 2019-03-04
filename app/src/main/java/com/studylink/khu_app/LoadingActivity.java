13fffae6464971a1e97d5701154badb4680f19a2
smarto <mtw9411@naver.com>
2019-03-04 18:48:27 +0900

    Initial commit

---
+++ app/src/main/java/com/studylink/khu_app/LoadingActivity.java
@@ -0,0 +1,23 @@
+package com.studylink.khu_app;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
