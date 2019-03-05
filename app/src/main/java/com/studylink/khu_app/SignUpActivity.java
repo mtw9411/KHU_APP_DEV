d676fe1e92464e46ee629da122f4a5d1bdfc4929
smarto <mtw9411@naver.com>
2019-03-05 17:15:26 +0900

    아이디 찾기

--- app/src/main/java/com/studylink/khu_app/SignUpActivity.java
+++ app/src/main/java/com/studylink/khu_app/SignUpActivity.java
@@ -1,12 +1,30 @@
 package com.studylink.khu_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextView sign_up_next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        sign_up_next.findViewById(R.id.sign_up_next);
+
+        sign_up_next.setClickable(true);
+        sign_up_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SignUpFinalActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
