e8f807eaec18ae0bfd86c3c503abaf7e069ee044
nakkwan <cvvc1997@gmail.com>
2019-03-07 18:08:56 +0900

    이메일 형식설명 비밀번호 형식, 체크

--- app/src/main/java/com/studylink/khu_app/SignUpFinalActivity.java
+++ app/src/main/java/com/studylink/khu_app/SignUpFinalActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -18,6 +20,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Pattern;
+
 public class SignUpFinalActivity extends AppCompatActivity {
 
     private TextView confirm_btn_SMS;
@@ -29,10 +33,8 @@ public class SignUpFinalActivity extends AppCompatActivity {
     private EditText signupPassword;
     private EditText signupPassword_check;
     private CheckBox signupCheckBox1;
-    private Button signupCheckBox2;
+    private CheckBox signupCheckBox2;
     private boolean password_check = false;
-    private boolean emailverified = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +45,7 @@ public class SignUpFinalActivity extends AppCompatActivity {
         confirm_btn_SMS = (TextView)findViewById(R.id.confirm_btn_SMS);
         confirm_btn_email = (TextView) findViewById(R.id.confirm_btn_email);
         signupCheckBox1 = (CheckBox) findViewById(R.id.signupCheckBox1);
-        signupCheckBox2 = (Button) findViewById(R.id.signupCheckBox2);
+        signupCheckBox2 = (CheckBox) findViewById(R.id.signupCheckBox2);
 
         signupEmail = (EditText) findViewById(R.id.signupEmail);
         signupPassword = (EditText) findViewById(R.id.signupPassword);
@@ -62,7 +64,6 @@ public class SignUpFinalActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(!android.util.Patterns.EMAIL_ADDRESS.matcher(signupEmail.getText()).matches()) {
-                    emailverified = true;
                     Toast.makeText(SignUpFinalActivity.this, "이메일 형식이 아닙니다", Toast.LENGTH_SHORT).show();
                 } else{
                     Toast.makeText(SignUpFinalActivity.this, "이메일 형식이 확인되었습니다", Toast.LENGTH_SHORT).show();
@@ -70,12 +71,52 @@ public class SignUpFinalActivity extends AppCompatActivity {
             }
         });
 
-        /*signupCheckBox2.setOnClickListener(new View.OnClickListener() {
+        signupPassword_check.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                passwordcheck();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                signupCheckBox2.setChecked(false);
+
             }
-        });*/
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(signupPassword.getText().toString().equals(signupPassword_check.getText().toString())){
+                    signupCheckBox2.setChecked(true);
+                } else{
+                    signupCheckBox2.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!Pattern.matches("^[a-zA-Z0-9]{8,20}$", signupPassword.getText().toString())){
+                    Toast.makeText(SignUpFinalActivity.this,"비밀번호 형식을 지켜주세요.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        signupPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                signupCheckBox1.setChecked(false);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!Pattern.matches("^[a-zA-Z0-9]{8,20}$", signupPassword.getText().toString()))
+                {
+                    signupCheckBox1.setChecked(false);
+                } else{
+                    signupCheckBox1.setChecked(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         confirm_btn_SMS.setClickable(true);
         confirm_btn_SMS.setOnClickListener(new View.OnClickListener() {
@@ -106,17 +147,7 @@ public class SignUpFinalActivity extends AppCompatActivity {
                 });
     }
 
-    /*private void passwordcheck(){
-        String pw1 = signupPassword.getText().toString();
-        String pw2 = signupPassword_check.getText().toString();
-        if(pw1 == pw2){
-            password_check = true;
-            Toast.makeText(this, "확인", Toast.LENGTH_SHORT).show();
-        } else{
-            password_check = false;
-            Toast.makeText(this, "비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show();
-        }
-    }*/
+
 
     @Override
     public void onStart() {
