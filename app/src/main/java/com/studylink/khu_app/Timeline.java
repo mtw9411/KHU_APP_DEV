6acb26f23c0c54247b86db33eae87e0dd1b254e5
smarto <mtw9411@naver.com>
2019-03-11 18:21:34 +0900

    메인페이지 구현-1

--- app/src/main/java/com/studylink/khu_app/Timeline.java
+++ app/src/main/java/com/studylink/khu_app/Timeline.java
@@ -1,10 +1,11 @@
 package com.studylink.khu_app;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Timeline extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
