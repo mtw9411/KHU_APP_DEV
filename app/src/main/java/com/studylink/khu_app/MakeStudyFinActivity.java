68411eef8b7902f9997333c9763fde9580b05b3a
yongyongwon <joyw9312@gmail.com>
2019-03-22 20:10:58 +0900

    roomDTO 액티비티를 이용하여 방 정보를 구조화 함 방을 이제 어느정도 구색을 갖춘 채로 파이어베이스에 올릴 수 있게 되었음

--- app/src/main/java/com/studylink/khu_app/MakeStudyFinActivity.java
+++ app/src/main/java/com/studylink/khu_app/MakeStudyFinActivity.java
@@ -1,12 +1,23 @@
 package com.studylink.khu_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 
 public class MakeStudyFinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makestudy_fin);
+
     }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
 }
